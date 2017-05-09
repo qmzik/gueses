13f07c2821ed2447f7c164104d036b95583be358
qmzik <acke208@yandex.ru>
2017-05-09 16:21:17 +0500

    added first page

--- landing/urls.py
+++ landing/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 from landing import views
 
 urlpatterns = [
-    url(r'^landing/', views.landing, name='landing'),
+    url(r'^redbutton/', views.landing, name='first'),
 ]
--- landing/views.py
+++ landing/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def landing(request):
-    return render(request, 'landing/landing.html', locals())
+    return render(request, 'landing/first.html', locals())
