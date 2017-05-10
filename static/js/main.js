760800e74721da5d001f367fcc4619cd585e4e6b
qmzik <acke208@yandex.ru>
2017-05-10 21:11:26 +0500

    added animated background

---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Никита on 10.05.2017.
+ */
+
+$("body").vegas({
+    slides: [
+        { src: "http://respek.info/postimageswatermark/1168/2_full.jpg" },
+        { src: "http://murlo.org/ru/wp-content/uploads/2016/07/Самые-красивые-породы-кошек-британская-короткошерстная.jpg" },
+        { src: "http://www.gandex.ru/upl/oboi/u1752_6263_04.jpg" },
+        { src: "http://mirpozitiva.ru/uploads/posts/2016-10/1476878736_04545.jpg" }
+    ]
+});
